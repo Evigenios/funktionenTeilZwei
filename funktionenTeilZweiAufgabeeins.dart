3606ab1d8b633e50739fbf85489d0056c6cfeed5
Evigenios <e.chontzidis@web.de>
2025-03-13 11:14:11 +0100

    AlleAufgaben

--- funktionenTeilZweiAufgabeeins.dart
+++ funktionenTeilZweiAufgabeeins.dart
@@ -10,6 +10,49 @@ int textlaenge(String text) {
   return text.length;
 }
 
+enum Vokale { A, E, U, O, I, a, e, u, i }
+
+int vokaleAnzahl(String text) {
+  int count = 0;
+  for (int i = 0; i < text.length; i++) {
+    if (text[i] == "A") {
+      count++;
+    }
+
+    if (text[i] == "a") {
+      count++;
+    }
+
+    if (text[i] == "E") {
+      count++;
+    }
+
+    if (text[i] == "e") {
+      count++;
+    }
+
+    if (text[i] == "O") {
+      count++;
+    }
+    if (text[i] == "o") {
+      count++;
+    }
+    if (text[i] == "U") {
+      count++;
+    }
+    if (text[i] == "u") {
+      count++;
+    }
+    if (text[i] == "I") {
+      count++;
+    }
+    if (text[i] == "i") {
+      count++;
+    }
+  }
+  return count;
+}
+
 void main() {
   //Ausgabe Aufgabe 1
   print(dreiVariablenSumme(5, 5, 5));
@@ -22,4 +65,10 @@ void main() {
   print(textlaenge("Hallo "));
   print(textlaenge("Welt"));
   print(textlaenge("ABCDEFGH IJ"));
+
+  //ausgabe Aufgabe 3
+  print("+++++++++++++++++++++++++++++++++++");
+  print(vokaleAnzahl("Indianer"));
+  print(vokaleAnzahl("Ahoi"));
+  print(vokaleAnzahl("AaEeIiUu"));
 }
